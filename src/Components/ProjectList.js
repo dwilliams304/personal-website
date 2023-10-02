8c751a00697d05dd6269b6d7c413b6ac539bdec0
dwilliams304 <dwilliams0434@gmail.com>
2023-10-02 16:07:43 -0700

    wip

--- src/Components/ProjectList.js
+++ src/Components/ProjectList.js
@@ -1,33 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import Project from './Project'
 import { Link } from "react-router-dom";
 
-const sampleProject = {
-    projName: 'Test Project',
-    projDescription: 'This is just a test description',
-    img: 'thumb',
-    languages: ['JS', 'C#', 'Python'],
-    projType: ['Website', 'App', 'Game'],
-    extLink: ''
-}
+
 
 export default function ProjectList(props){
-    const {projects} = props;
+    // const projects = props.projects;
+    const [projects, setProjects] = useState(props.projects);
+    // const [filters, setFilters] = useState([]);
+    const filters = [];
+
+
+
+    
+    const addFilter = e => {
+        const { checked, value } = e.target;
+        console.log(`Is checked ${checked} // Name: ${value}`)
+        if(checked) filters.push(value);
+        else {
+            const i = filters.indexOf(value);
+            filters.splice(i, 1);
+        }
+        console.log(filters);
+    }
+
     return(
         <div>
             <h2 className='project-headers page-title'>Here are some of my projects.</h2>
             <div className='filters'>
             <fieldset>
                 <legend>Language Used</legend>
-                <label><input type="checkbox" name="filter" value="csharp" />&nbsp;C#</label>
-                <label><input type="checkbox" name="filter" value="javascript" />&nbsp;JavaScript</label>
-                <label><input type="checkbox" name="filter" value="python" />&nbsp;Python</label>
+                <label><input type="checkbox" name="filter" value="c#" onChange={addFilter}/>&nbsp;C#</label>
+                <label><input type="checkbox" name="filter" value="js" onChange={addFilter}/>&nbsp;JavaScript</label>
+                <label><input type="checkbox" name="filter" value="python" onChange={addFilter}/>&nbsp;Python</label>
                 </fieldset>
                 <fieldset>
                 <legend>Project Type</legend>
-                <label><input type="checkbox" name="filter" value="websites" />&nbsp;Websites</label>
-                <label><input type="checkbox" name="filter" value="apps" />&nbsp;Apps</label>
-                <label><input type="checkbox" name="filter" value="games" />&nbsp;Games</label>
+                <label><input type="checkbox" name="filter" value="website" onChange={addFilter}/>&nbsp;Website</label>
+                <label><input type="checkbox" name="filter" value="app" onChange={addFilter}/>&nbsp;App</label>
+                <label><input type="checkbox" name="filter" value="game" onChange={addFilter}/>&nbsp;Game</label>
             </fieldset>
         </div>
             <div className='projects-container'>
