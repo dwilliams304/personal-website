fb1cbde1a8677cc539a5165acd7ce64c7db001b5
dwilliams304 <d.williams0434@gmail.com>
2023-10-10 16:55:58 -0700

    Home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,11 +9,29 @@ export default function Home(){
 
     return(
         <div className='home'>
-            <h2>Hello</h2>
+            <h2>Hello.</h2>
             <img className="img-coin" src="https://shorturl.at/cPT03" alt="headshot"/>
-            <h3>My name is Davis.</h3>
-            <p>Welcome to my portfolio.</p>
-            <p>Here is some more basic information about me, and who I am.</p>
+            <div className="container ">
+                <div className="introduction about-me">
+                    <h2>My name is Davis.</h2>
+                    <p>Welcome to my portfolio.</p>
+                    <p>I am an aspiring Fullstack Web Developer with a strong foundation in customer
+                    service and a passion for creating user-centric web applications.<br/> Proficient in
+                    JavaScript and ReactJS, actively developing skills through self-study and
+                    ongoing learning.</p>
+
+                    <p>This website is designed to show off all projects I've worked on and is also to
+                        help showcase some of my skills with designing, programming, and anything <br/> else
+                        related to web development!
+                    </p>
+                </div>
+                <div className="skills about-me">
+                    <h3>Here Are Some of My Skills:</h3>
+                    <p>JavaScript, C#, ReactJS, Redux, NodeJS, HTML, CSS, REST APIs, Frontend/Backend Development <br/>
+                        Game Development, Unity Engine, Unreal Engine</p>
+                </div>
+
+            </div>
             <button className='projects-btn' onClick={() => nav('/projects')}>Projects //</button>
         </div>
     )
