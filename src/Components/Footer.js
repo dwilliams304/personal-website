a8f3885ba6b8f2e43b3e42566fff84f0176b3b71
dwilliams304 <d.williams0434@gmail.com>
2023-10-10 12:41:03 -0700

    wip

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 
 function Footer(props){
@@ -7,8 +8,12 @@ function Footer(props){
     return(
         <footer>
             <h2>Property of Davis Williams</h2>
-            <button className="social-icn" onClick={() => openInNewWindow(github)}>github</button>
-            <button className="social-icn" onClick={() => openInNewWindow(linkedin)}>linkedin</button>
+            <button className="social-icn" onClick={() => openInNewWindow(github)}>
+                <FaGithub />
+            </button>
+            <button className="social-icn" onClick={() => openInNewWindow(linkedin)}>
+                <FaLinkedin />
+            </button>
         </footer>
     )
 }
