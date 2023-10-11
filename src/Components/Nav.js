550368772f1eeb1305a5dac9e0c36d4e2b502330
dwilliams304 <d.williams0434@gmail.com>
2023-10-10 17:34:32 -0700

    Homepage, navbar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,21 +1,24 @@
 import React, {useRef, useState, useEffect} from "react";
 import { FaBars, FaTimes } from 'react-icons/fa'; 
+import { headers } from '../Helpers/RandomHeaders';
 
 
 export default function NavBar(props){
     const navRef = useRef();
-    const [headerText, setHeaderText] = useState('daviswilliams.portfolio');
+    const [headerText, setHeaderText] = useState('daviswilliams');
     const showNav = () => {
         navRef.current.classList.toggle('responsive_nav');
     }
 
     const changeText = () => {
-        setHeaderText('dvaisliwlsima.protfolio');
+        var randomNumber = Math.floor(Math.random() * headers.length);
+
+        setHeaderText(headers[randomNumber]);
     }
 
     return(
         <header>
-            <h3 onClick={changeText}>{headerText}</h3>
+            <h3 onClick={changeText}>{headerText}.portfolio</h3>
             <nav ref={navRef}>
                 <a href='/'>Home</a>
                 <a href='/projects'>Projects</a>
