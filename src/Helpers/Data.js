d855c1599d2a47c0186b666070598e50cc914071
dwilliams304 <d.williams0434@gmail.com>
2023-10-28 14:22:54 -0700

    Update projects data

--- src/Helpers/Data.js
+++ src/Helpers/Data.js
@@ -96,6 +96,53 @@ const Data =
       imgs: ['', '', '',]
     }
   },
+  {
+    projID: 2,
+    projName: 'Weather App',
+    projDescription: 'A React website that can tell you the weather/conditions of any city',
+    img: 'https://raw.githubusercontent.com/dwilliams304/personal-website/main/public/assets/imgs/Projectcard/portfoliowebsiteprojectcard.PNG',
+    categories: {
+      language: 'js',
+      type: 'website'
+    },
+    pageData: {
+      bannerImg: 'https://raw.githubusercontent.com/dwilliams304/personal-website/main/public/assets/imgs/Projectcard/portfoliowebsiteprojectcard.PNG',
+      dateStarted: '10/28/2023',
+      status: 'Completed',
+      websiteLink: '',
+      githubLink: 'https://github.com/dwilliams304/weather-app',
+      fullDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit \
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat \
+      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\
+      \
+      \nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit \
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat \
+      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\
+      \
+      \nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit \
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat \
+      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \
+      \
+      \nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit \
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat \
+      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \
+      \
+      \nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
+      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit \
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat \
+      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      imgs: ['', '', '',]
+    }
+  },
 ]
 
   export default Data;
