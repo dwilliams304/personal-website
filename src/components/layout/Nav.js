58ef8dbbef42acfcd7fc86ba890c41b5f22849b1
dwilliams304 <d.williams0434@gmail.com>
2023-12-06 10:21:28 -0700

    Update NavBar

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,10 +1,13 @@
 import React, {useRef, useState} from "react";
+import { useNavigate } from 'react-router';
+import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from 'react-icons/fa'; 
 import { headers } from '../../data/RandomHeaders';
 
 
 export default function NavBar(props){
     const navRef = useRef();
+    const nav = useNavigate();
     const [headerText, setHeaderText] = useState('daviswilliams');
     const showNav = () => {
         navRef.current.classList.toggle('responsive_nav');
@@ -20,9 +23,10 @@ export default function NavBar(props){
         <header>
             <h3 onClick={changeText}>{headerText}.portfolio</h3>
             <nav ref={navRef}>
-                <a href='/'>Home</a>
-                <a href='/projects'>Projects</a>
-                <a href='/' onClick={() => props.openLink("https://forms.gle/fdhfSxEpDU7rsWFX8")}>Contact</a>
+                {/* <a href='/'>Home</a> */}
+                <Link to='/'>Home</Link>
+                <Link to='/projects'>Projects</Link>
+                <span onClick={() => props.openLink("https://forms.gle/fdhfSxEpDU7rsWFX8")}>Contact</span>
                 <button className='nav-btn nav-close-btn' onClick={showNav}>
                     <FaTimes />
                 </button>
